cc72939ece05ede3506a8ad2ed0d6df91a6d25bc
Tiago Santos <tfsantosbr@gmail.com>
2020-04-26 02:02:32 -0300

    =)

--- src/Wombo.Api/Controllers/StatusController.cs
+++ src/Wombo.Api/Controllers/StatusController.cs
@@ -23,7 +23,7 @@ namespace Wombo.Api.Controllers
             return Ok(new
             {
                 Project = "Wombo API",
-                Version = "0.5.0",
+                Version = "0.6.0",
                 Environment = $"{_environment.EnvironmentName}",
                 Status = "Running",
                 AppSettings = new 
